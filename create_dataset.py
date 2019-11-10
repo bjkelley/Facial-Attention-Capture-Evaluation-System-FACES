8059d2057a7f8161437168374e92097d51eb537e
Huyen Pham <thanhhuyen2995@gmail.com>
2019-11-10 11:41:39 -0800

    wrap everything into a class

--- create_dataset.py
+++ create_dataset.py
@@ -1,34 +1,55 @@
-import glob
+import glob, os
 import matplotlib.pyplot as plt
 import numpy as np
 import tensorflow as tf
 import scipy.misc
 from scipy import ndimage
 
-def emotion(x):
-    return {
-        0: 'neutral frontal',
-        1: 'joy',
-        2: 'sadness',
-        3: 'surprise',
-        4: 'anger',
-        5: 'disgust',
-        6: 'fear',
-        7: 'opened',
-        8: 'closed',
-        9: 'kiss'
-    }[x]       
+class DataLoader:
+    def __init__(self, datapath = ""):
+        self.datapath = datapath
 
-data = []
-for i in sorted(glob.glob("facesdb/**/bmp/*.bmp", recursive=True)):
-    data.append(plt.imread(i))
+    def get_emotion(self, x):
+        return {
+            0: 'neutral frontal',
+            1: 'joy',
+            2: 'sadness',
+            3: 'surprise',
+            4: 'anger',
+            5: 'disgust',
+            6: 'fear',
+            7: 'opened',
+            8: 'closed',
+            9: 'kiss'
+        }[x]
 
-data = np.stack(data)
+    def getDataset(self):
+        datapath = os.path.join(self.datapath, "facesdb/**/bmp/*.bmp")
+        data = []
+        for i in glob.glob(datapath, recursive=True):
+            data.append(plt.imread(i))
 
-y = []
-image = 0 
-for i in range(0,36):
-    for j in range(0,10):
-        y.append(emotion(j))
+        data = np.stack(data)
+        y = self.create_label_vector()
+        dataset = tf.data.Dataset.from_tensor_slices((data, y))
 
-dataset = tf.data.Dataset.from_tensor_slices((data, y))
+        return dataset
+
+    def create_label_vector(self):
+        # y is label vector
+        y = []
+        image = 0
+        for i in range(0,36):
+            for j in range(0,10):
+                y.append(self.get_emotion(j))
+
+        return y
+
+dataLoader = DataLoader()
+dataset = dataLoader.getDataset()
+
+'''Debugging purpose
+for img, label in dataset:
+    plt.imshow(img.numpy())
+    plt.show()
+'''
