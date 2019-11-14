f908ecc4828e08b10942125f771de6f6ff72475d
Huyen Pham <thanhhuyen2995@gmail.com>
2019-11-14 14:55:13 -0800

    removed salt and pepper

--- image_mod_functions.py
+++ image_mod_functions.py
@@ -24,26 +24,3 @@ def add_speckle(image):
     gauss = gauss.reshape(row,col,ch)
     noisy_image = image + image * gauss
     return noisy_image
-
-def add_saltnpepper_noise(image):
-    out = np.zeros(image.shape)
-    # salt coordinates
-    coords = [np.random.randint(0,26,50), np.random.randint(0,26,50)]
-
-    # mask - 0 are regions where salt can be applied, otherwise don't touch
-    mask = np.zeros(out.shape)
-    mask[:13,:13] = 1
-    mask[-13:,-13:] = 2
-
-    # where does the salt coordinates land on the mask
-    a = mask[coords]
-
-    # find points where mask is 0
-    b = np.nonzero(a==0)
-
-    # copy from coords only where mask is 0
-    valid_coords = np.array(coords)[:,b]
-
-    # apply salt on valid coordinates
-    out = out.numpy()
-    out[valid_coords.tolist()]=1
