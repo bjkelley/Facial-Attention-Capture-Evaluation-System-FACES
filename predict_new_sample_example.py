2e0ab32e4fb28a00de9ce0df89fcfb370371fdc5
mjnur <mjnur@ucdavis.edu>
2019-11-19 03:43:52 -0800

    completed example prediction script

--- predict_new_sample_example.py
+++ predict_new_sample_example.py
@@ -6,14 +6,22 @@ import numpy as np
 myLoader = LoadModel(model_type='cnn2', input_shape=(60,60,1))
 
 # read in example image to be classified
-image = plt.imread('facesdb/s001/bmp/s001-00_img.bmp')
+image = plt.imread('facesdb/s003/bmp/s003-00_img.bmp')
 y_true = 'neutral frontal'
+
+plt.imshow(np.squeeze(image))
+plt.title('raw image')
+plt.show() 
+
+# do enough preprocessing for model input(facial detection, grayscale, pixel resizing)
+image = myLoader.preprocess(image)
+
+plt.imshow(np.squeeze(image))
+plt.title('after preprocessing')
+plt.show() 
 
 # classify image (does preprocessing for it)
 top_class, top_k_classes, top_k_probs = myLoader.classify(image, k_most_confident_classes=3)
-
-plt.imshow(np.squeeze(image))
-plt.show()
 
 print(f"true class: {y_true}")
 print(f"predicted class: {top_class}")
