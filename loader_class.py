f890630cd770df113636d6371661e708c7b407af
mjnur <mjnur@ucdavis.edu>
2019-11-19 03:43:08 -0800

    fixed loader class bugs

--- loader_class.py
+++ loader_class.py
@@ -23,93 +23,90 @@ dependencies = {'top3_acc' : top3_acc}
 
 
 class LoadModel():
-	'''
-	LoadModel: class to load pretrained image emotion classifier
+    '''
+    LoadModel: class to load pretrained image emotion classifier
 
-	contains functions to:
-		- return predicted emotion string
-		- preprocess an image to get it ready for classifiaction
-		- return top prediction and/or top3 predictions
-	'''
-	def __init__(self, model_type="cnn2", input_shape=(60,60, 1)):
-		# initialize object attributes
-		self.model_type = model_type.lower()
-		self.input_shape = input_shape
+    contains functions to:
+        - return predicted emotion string
+        - preprocess an image to get it ready for classifiaction
+        - return top prediction and/or top3 predictions
+    '''
+    def __init__(self, model_type="cnn2", input_shape=(60,60, 1)):
+        # initialize object attributes
+        self.model_type = model_type.lower()
+        self.input_shape = input_shape
 
-		# load OpenCV face detection cascade for preprocessing steps
-	    self.face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+        # load OpenCV face detection cascade for preprocessing steps
+        self.face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
-		# load in correct model
-		if self.model_type == "cnn2"
-			self.model = keras.models.load_model('trained_models/cnn2.h5', custom_objects=dependencies)
-		elif self.model_type == "cnn1":
-			self.model = keras.models.load_model('trained_models/cnn2.h5', custom_objects=dependencies)
-		elif self.model_type == "fastsvm":
-			self.model = keras.models.load_model('trained_models/fastsvm.h5', custom_objects=dependencies)
-		else:
-			raise Exception(f"Model type {model_type} not found. Try another model.")
+        # load in correct model
+        if self.model_type == "cnn2":
+            self.model = keras.models.load_model('trained_models/cnn2.h5', custom_objects=dependencies)
+        elif self.model_type == "cnn1":
+            self.model = keras.models.load_model('trained_models/cnn2.h5', custom_objects=dependencies)
+        elif self.model_type == "fastsvm":
+            self.model = keras.models.load_model('trained_models/fastsvm.h5', custom_objects=dependencies)
+        else:
+            raise Exception(f"Model type {model_type} not found. Try another model.")
 
-	def get_emotion(one_hot_idx):
-		'''returns emotion string that corresponds to the one-hot-encoded index'''
-		return {
-			0: 'neutral frontal',
-			1: 'joy',
-			2: 'sadness',
-			3: 'surprise',
-			4: 'anger',
-			5: 'disgust',
-			6: 'fear',
-			7: 'opened',
-			8: 'closed',
-			9: 'kiss'
-		}[one_hot_idx]
+    def get_emotion(self, one_hot_idx):
+        '''returns emotion string that corresponds to the one-hot-encoded index'''
+        return {
+            0: 'neutral frontal',
+            1: 'joy',
+            2: 'sadness',
+            3: 'surprise',
+            4: 'anger',
+            5: 'disgust',
+            6: 'fear',
+            7: 'opened',
+            8: 'closed',
+            9: 'kiss'
+        }[one_hot_idx]
 
-	def preprocess(img):
-		'''preprocesses image accordingly so that model can take it as input
-		- use OpenCV face detector and convert to grayscale
-		- resize image
-		'''
+    def preprocess(self, img):
+        '''preprocesses image accordingly so that model can take it as input
+        - use OpenCV face detector and convert to grayscale
+        - resize image
+        '''
 
-	    # Convert into grayscale
-	    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-	    
-	    # Detect faces, extract them
-	    faces = self.face_cascade.detectMultiScale(gray, 1.1, 4)
-	    x, y, w, h = faces[0]
-	    img = gray[y:y+h, x:x+w]
-	    
-	    # reshape so that there's 1 channel
-	    img = img.reshape(h, w, 1)
-	    
-	    # scale image to input_shape width x height
-	    img = standardize_image(img, self.input_shape[0], self.input_shape[1])
+        # Convert into grayscale
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-	    return img
-	   
+        # Detect faces, extract them
+        faces = self.face_cascade.detectMultiScale(gray, 1.1, 4)
+        x, y, w, h = faces[0]
+        img = gray[y:y+h, x:x+w]
 
-	def classify(img, k_most_confident_classes=3):
-		'''takes in img
-		returns: 
-			- predicted class string
-			- top k (3 by default) most confident emotion string predictions
-			- the corresponding k (3 by default) emotion probailities
-		'''
-		
-		# do preprocessing (facial detection, grayscale, pixel resizing)
-		img = preprocess(img)
+        # reshape so that there's 1 channel
+        img = img.reshape(h, w, 1)
 
-		# plug into model to get array of probabilities
-		probs = self.model.predict_proba(img)
+        # scale image to input_shape width x height
+        img = standardize_image(img, self.input_shape[0], self.input_shape[1])
 
-		# sort probability indices from highest to lowest probabilitiy
-		sorted_indices = np.argsort(probs)[::-1]
+        return np.array([img])
+    
+    def classify(self, img, k_most_confident_classes=3):
+        '''takes in img
+        returns: 
+            - predicted class string
+            - top k (3 by default) most confident emotion string predictions
+            - the corresponding k (3 by default) emotion probailities
+        '''
 
-		# get sorted emotions
-		sorted_emotions = [get_emotion(idx) for idx in sorted_indices]
+        # plug into model to get array of probabilities
+        probs = self.model.predict_proba(img).reshape(-1)
+        
+        # sort probability indices from highest to lowest probabilitiy
+        sorted_indices = np.argsort(probs)[::-1]
 
-		# get sorted probabilities
-		sorted_probabilities = [probs[idx] for idx in sorted_indices]
+        # get sorted emotions
+        sorted_emotions = [self.get_emotion(idx) for idx in sorted_indices]
 
-		return sorted_emotions[0], 
-			   sorted_emotions[:k_most_confident_classes],
-			   sorted_probabilities[:k_most_confident_classes]
+        # get sorted probabilities
+        sorted_probabilities = [probs[idx] for idx in sorted_indices]
+
+        return (sorted_emotions[0], 
+                sorted_emotions[:k_most_confident_classes],
+                sorted_probabilities[:k_most_confident_classes]
+               )
